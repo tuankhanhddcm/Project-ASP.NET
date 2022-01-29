9bf99b8fb5b458660a9cadb0e7a7497e6afe5bbd
tuankhanhddcm <khanhprince27@gmail.com>
2022-01-29 15:40:26 +0700

    Add project files.

---
+++ KStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
